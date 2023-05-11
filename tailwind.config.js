0c6b3170af09b2e1cfe521c540d0e0c6de6706a1
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-05-11 16:23:39 +0530

    add daisyui and complete cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['valentine'],
+  },
 }
