f40ef89ced1db6144d8f6e8165b061157cd6fd8a
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-05-12 16:55:18 +0530

    add react router + minor config/rule tweak

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import { Home, Game, NotFound } from 'pages'
 
 function App() {
   return (
-    <div>
-      <h1>The start!</h1>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/game' element={<Game />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Router>
+    </React.Fragment>
   )
 }
 
