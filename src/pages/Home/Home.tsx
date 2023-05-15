2a418a6fbd813d0a3793b9165d697e4148ea3a28
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-05-15 12:47:33 +0530

    write a basic outline for the Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,34 @@
+import { Link } from 'react-router-dom'
+
 const Home = () => {
   return (
-    <main className='mx-auto max-w-7xl px-6 my-8'>
+    <main className='overflow-x-hidden'>
       <h3 className='text-7xl text-center font-bold'>Migawe</h3>
+      <section>
+        <h4>Section 1 - Hero Section</h4>
+        <p>Display the game here</p>
+        <Link to='/game' className='btn btn-primary btn-wide'>
+          Play the game
+        </Link>
+        Enter with $1USDT
+      </section>
+      <section>
+        <h4>Section 2</h4>
+        <p>Explain what Migawe is</p>
+      </section>
+      <section>
+        <h4>Section 3</h4>
+        <p>Deep dive into reward and stakeholder pool</p>
+        Ecosystem
+      </section>
+      <section>
+        <h4>Section 4</h4>
+        <p>Deep dive into branding</p>
+      </section>
+      <section>
+        <h4>Section 5</h4>
+        <p>Deep dive engagement, indapp games</p>
+      </section>
     </main>
   )
 }
