fba5299694b779c1177aa11ef0f6187554cfaceb
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-05-16 13:00:16 +0530

    create assets dir and include illustrations to be used on Hero Section of the home page

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import heroSectionGameIllustartion from './hero-section-game-illustration.png'
+import heroSectionPrimaryIllustartion from './hero-section-primary-illustration.png'
+import heroSectionSecondaryIllustartion from './hero-section-secondary-illustration.png'
+
+export {
+  heroSectionGameIllustartion,
+  heroSectionPrimaryIllustartion,
+  heroSectionSecondaryIllustartion,
+}
