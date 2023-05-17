e8dd622b99aaa35b23051f450fc70a0ca24c549e
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-05-17 13:06:43 +0530

    fix non existent export error

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,8 +2,4 @@ import heroSectionGameIllustartion from './hero-section-game-illustration.png'
 import heroSectionPrimaryIllustartion from './hero-section-primary-illustration.png'
 import heroSectionSecondaryIllustartion from './hero-section-secondary-illustration.png'
 
-export {
-  heroSectionGameIllustartion,
-  heroSectionPrimaryIllustartion,
-  heroSectionSecondaryIllustartion,
-}
+export { heroSectionPrimaryIllustartion, heroSectionSecondaryIllustartion }
