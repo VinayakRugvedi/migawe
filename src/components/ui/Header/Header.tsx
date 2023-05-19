8801ae3870aad5137d58da737f2733127070b301
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-05-19 14:19:35 +0530

    update Header style and semantic

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -2,18 +2,21 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header className='h-[125px] fixed top-0 inset-x-0 px-12'>
-      <div className='h-full flex justify-between items-center uppercase'>
+    <header
+      className='h-[125px] fixed top-0 inset-x-0 px-12 z-30 bg-opacity-90 backdrop-blur transition-all duration-100 
+    bg-base-100'
+    >
+      <nav className='h-full flex justify-between items-center uppercase'>
         <Link to='/' className='hover:text-primary'>
           <h3 className='text-2xl font-bold'>MIGAWE</h3>
         </Link>
         <div>
-          <Link className='mr-6 font-normal hover:text-primary' to='/game'>
+          <Link className='mr-6 font-normal hover:text-primary' to='/ronins-gambit'>
             Let&apos;s Play
           </Link>
           <button className='btn'>Connect Wallet</button>
         </div>
-      </div>
+      </nav>
     </header>
   )
 }
