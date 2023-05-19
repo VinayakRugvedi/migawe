1369f7cb07cec17f8a1690a7ed7a152552637ba0
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-05-19 23:33:54 +0530

    minor style fix

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <main className='w-screen h-screen flex flex-col justify-center items-center p-6'>
+    <main className='h-screen flex flex-col justify-center items-center p-6'>
       <div className='mb-6 text-center'>
         <h3 className='text-7xl'>404</h3>
         <p>The page you are looking for cannot be found.</p>
       </div>
-      <Link className='btn btn-primary btn-wide' to='/'>
+      <Link className='btn btn-wide' to='/'>
         Take me Home
       </Link>
     </main>
