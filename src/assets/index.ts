c974ae4da42919ba0fb2b2869be94e8ef68ae331
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-05-29 18:50:46 +0530

    include migawe logo and export

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,11 @@
 import heroSectionPrimaryIllustartion from './hero-section-primary-illustration.png'
 import heroSectionSecondaryIllustartion from './hero-section-secondary-illustration.png'
 import gameTitleIllustration from './game-title-illustration.webp'
+import Logo512 from './migawe-logo-512.png'
 
-export { heroSectionPrimaryIllustartion, heroSectionSecondaryIllustartion, gameTitleIllustration }
+export {
+  heroSectionPrimaryIllustartion,
+  heroSectionSecondaryIllustartion,
+  gameTitleIllustration,
+  Logo512,
+}
