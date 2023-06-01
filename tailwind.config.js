4a64ae86a96cd432f7df131a3b902a21e8830fc2
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-06-01 23:16:24 +0530

    add support for new font family - press start; for game play

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        game: ['"Press Start 2P"', 'cursive'],
+      },
+    },
   },
   plugins: [require('daisyui')],
   daisyui: {
