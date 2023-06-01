168b495ea21a56a302e5bdaacd32c6dc098c8a92
rudrakpatra <rudrakpatracodes@gmail.com>
2023-06-02 02:53:37 +0530

    updated videos and music

--- src/game-play-assets/index.ts
+++ src/game-play-assets/index.ts
@@ -1,8 +1,13 @@
 import idleVideo from './idle.mp4'
 import winVideo from './win.mp4'
-import loseVideo from './lose.mp4'
+import loseVideo from './loose.mp4'
 import tieVideo from './tie.mp4'
 import introSequenceImage from './intro-sequence.png'
-import introSequenceMusic from './intro-sequence.mp3'
+import introSequenceMusic from './music/normal.mp3'
 
-export { idleVideo, winVideo, loseVideo, tieVideo, introSequenceImage, introSequenceMusic }
+import normalMusic from './music/normal.mp3'
+import intenseMusic from './music/intense.mp3'
+import fillMusic from './music/shakuhachi.mp3'
+
+
+export { idleVideo, winVideo, loseVideo, tieVideo, introSequenceImage, introSequenceMusic, normalMusic, intenseMusic, fillMusic }
