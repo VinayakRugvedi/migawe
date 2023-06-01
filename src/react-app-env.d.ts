713deda44c342d1e7c326fe3628c526e18c6242f
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-06-01 23:14:45 +0530

    help ts understand mp4 and mkv video importS and not throw an error

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare module '*.mp4' {
+  const src: string
+  export default src
+}
+
+declare module '*.mkv' {
+  const src: string
+  export default src
+}
