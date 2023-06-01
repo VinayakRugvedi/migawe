72ad60c79e4c1e036543fe6e2e6cc33d9c6bc4f9
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-06-01 23:44:27 +0530

    move game-play-assets outside to under src

---
+++ src/game-play-assets/index.ts
@@ -0,0 +1,8 @@
+import idleVideo from './idle.mp4'
+import winVideo from './win.mp4'
+import loseVideo from './lose.mp4'
+import tieVideo from './tie.mp4'
+import introSequenceImage from './intro-sequence.png'
+import introSequenceMusic from './intro-sequence.mp3'
+
+export { idleVideo, winVideo, loseVideo, tieVideo, introSequenceImage, introSequenceMusic }
