0fc82c8ee8222a101fbb72dd09084911cf384727
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-06-08 13:15:33 +0530

    show GameWallet and update spacing

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 
 import { ThirdWebConnectWalletButton } from 'components/ui'
 import { Logo512 } from 'assets'
+import { GameWallet } from './components'
 
 const Header = () => {
   return (
@@ -9,7 +10,7 @@ const Header = () => {
       className='h-[125px] fixed top-0 inset-x-0 px-12 z-30 bg-opacity-70 backdrop-blur transition-all duration-100 
     bg-base-100'
     >
-      <nav className='h-full flex justify-between items-center uppercase'>
+      <nav className='h-full flex justify-between items-center'>
         <Link to='/' className='hover:text-primary flex items-center'>
           <img
             src={Logo512}
@@ -17,10 +18,13 @@ const Header = () => {
             width='24'
             className='mr-2 animate-[spin_5s_ease-in-out_infinite]'
           />
-          <h3 className='text-2xl font-bold'>MIGAWE</h3>
+          <h3 className='text-2xl font-bold uppercase'>MIGAWE</h3>
         </Link>
         <div className='flex items-center'>
-          <Link className='mr-6 font-normal hover:text-primary' to='/ronins-gambit'>
+          <div className='mr-12'>
+            <GameWallet />
+          </div>
+          <Link className='mr-12 font-normal uppercase hover:text-primary' to='/ronins-gambit'>
             Game
           </Link>
           <ThirdWebConnectWalletButton />
