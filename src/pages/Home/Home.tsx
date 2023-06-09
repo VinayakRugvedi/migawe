9ba0ff5caa1f40acb1a23487ed4d4a2c32ea6af6
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-06-09 15:09:02 +0530

    get rid of commented code and wrap up

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,32 +1,10 @@
-// import { Link } from 'react-router-dom'
-
 import { HeroSection, AboutSection } from './components'
 
 const Home = () => {
   return (
     <main className='overflow-x-hidden pb-[115px]'>
       <HeroSection />
-
       <AboutSection />
-
-      {/* <h3 className='text-7xl text-center font-bold'>Migawe</h3>
-      <section>
-        <h4>Section 2</h4>
-        <p>Explain what Migawe is</p>
-      </section>
-      <section>
-        <h4>Section 3</h4>
-        <p>Deep dive into reward and stakeholder pool</p>
-        Ecosystem
-      </section>
-      <section>
-        <h4>Section 4</h4>
-        <p>Deep dive into branding</p>
-      </section>
-      <section>
-        <h4>Section 5</h4>
-        <p>Deep dive engagement, indapp games</p>
-      </section> */}
     </main>
   )
 }
