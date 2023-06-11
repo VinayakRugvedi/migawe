f3b39e143290d32d5ba307f4b0098c16c197ca41
Raunaque97 <rpatra1997@gmail.com>
2023-06-12 03:41:20 +0530

    updated game contract addr

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,8 +4,8 @@ import rpsGameABI from './rpsGameABI.json'
 const UI = {
   HeaderHeightInPx: 125,
   tokenName: 'TST',
-  MINIMUM_BALANCE:10,
-  WAGER_AMOUNT:1
+  MINIMUM_BALANCE: 10,
+  WAGER_AMOUNT: 1,
 }
 
 const RONIN_GAMBIT = {
@@ -14,14 +14,12 @@ const RONIN_GAMBIT = {
 }
 
 const CONTRACTS = {
-  rpsGameAddress: '0xE890cFBc0741ef64770dE16C09051C624bC9271e',
+  rpsGameAddress: '0x97c4f6b220b06e63b36699dc068d7cd8e8e467d2',
   rpsGameABI: rpsGameABI,
   gameWalletAddress: '39bc12763882A3959E8BFf8A41c24f0381BCBF52',
   gameWalletABI: gameWalletABI,
   erc20Address: 'a69bD215aB75BDf55d4DAB9734c74fea212D7f4C', //TODO: get from gameWallet contract
   erc20ABI: erc20ABI,
 }
-
-
 
 export { UI, RONIN_GAMBIT, CONTRACTS }
