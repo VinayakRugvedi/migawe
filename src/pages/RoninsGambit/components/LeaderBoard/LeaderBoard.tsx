e254c21b3f10835d184add49ef14034ed72e00e9
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-06-22 17:41:05 +0530

    replace moment with dayjs, update leaderboard section UI + make it responsive

--- src/pages/RoninsGambit/components/LeaderBoard/LeaderBoard.tsx
+++ src/pages/RoninsGambit/components/LeaderBoard/LeaderBoard.tsx
@@ -1,5 +1,8 @@
 import { BigNumber } from 'ethers'
-import moment from 'moment'
+import dayjs from 'dayjs'
+import relativeTimePlugin from 'dayjs/plugin/relativeTime'
+dayjs.extend(relativeTimePlugin)
+
 import { UI } from 'utils/constants'
 interface PropTypes {
   leaderboard: {
@@ -14,15 +17,17 @@ const LeaderBoard = ({ leaderboard, endTime, rewardPool }: PropTypes) => {
   const leaderboardFiltered = leaderboard.filter(
     (player) => player.address !== '0x0000000000000000000000000000000000000000',
   )
-  const formattedTime = moment(endTime).fromNow()
+  const isLeaderBoardEmpty = leaderboardFiltered.length === 0 ? true : false
+  const formattedTime = dayjs(endTime).fromNow()
+
   return (
     <div className='mt-16 w-full'>
       <h4 className='text-2xl font-medium'>Leaderboard</h4>
       <div className='card mt-8'>
         <div className='text-xl font-medium'>
-          Top 5 players will receive bonus rewards at the end of each tournament !
+          Top 5 players will receive bonus rewards at the end of each tournament.
         </div>
-        <div className='flex flex-row'>
+        <div className='stats stats-vertical md:stats-horizontal shadow-lg bg-primary/10 mt-4'>
           {rewardPool ? (
             <div className='stat'>
               <div className='stat-title'>Reward pool</div>
@@ -30,38 +35,40 @@ const LeaderBoard = ({ leaderboard, endTime, rewardPool }: PropTypes) => {
                 {rewardPool} {UI.tokenName}
               </div>
             </div>
-          ) : (
-            ''
-          )}
+          ) : null}
           {endTime ? (
             <div className='stat'>
               <div className='stat-title'>Tournament ends</div>
               <div className='stat-value'>{formattedTime}</div>
             </div>
-          ) : (
-            ''
-          )}
+          ) : null}
         </div>
         <div className='shadow-xl rounded-md overflow-x-auto mt-8'>
           <div className='flex flex-col'>
-            <div className='flex bg-base-100 bg-neutral/20'>
+            <div className='flex bg-neutral/20'>
               <div className='w-[25%] p-2 text-center font-bold'>Rank</div>
               <div className='w-[50%] p-2 text-center font-bold'>Address</div>
               <div className='w-[25%] p-2 text-center font-bold'>Wins</div>
             </div>
-            {leaderboardFiltered.map((player, index) => {
-              return (
-                <div className='flex ' key={index}>
-                  <div className='w-[25%] p-2 text-center'>{index + 1}</div>
-                  <div className='w-[50%] p-2 text-center overflow-hidden overflow-ellipsis'>
-                    {player.address}
+            {!isLeaderBoardEmpty ? (
+              leaderboardFiltered.map((player, index) => {
+                return (
+                  <div className='flex items-center' key={index}>
+                    <div className='w-[25%] p-2 text-center'>
+                      <span className='badge font-medium'>{index + 1}</span>
+                    </div>
+                    <div className='w-[50%] p-2 text-center overflow-hidden overflow-ellipsis'>
+                      {player.address}
+                    </div>
+                    <div className='w-[25%] p-2 text-center'>
+                      {player.wins?.toString() || 'loading...'}
+                    </div>
                   </div>
-                  <div className='w-[25%] p-2 text-center'>
-                    {player.wins?.toString() || 'loading...'}
-                  </div>
-                </div>
-              )
-            })}
+                )
+              })
+            ) : (
+              <div className='flex flex-col items-center justify-center p-4'>No one here yet!</div>
+            )}
           </div>
         </div>
       </div>
