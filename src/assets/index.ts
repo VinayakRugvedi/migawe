85092ef045435fc8be2fa699241504fb782de635
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2023-07-10 23:54:06 +0530

    add both dark and light logo + cleanup

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,13 +2,15 @@ import heroSectionPrimaryIllustartion from './hero-section-primary-illustration.
 import heroSectionSecondaryIllustartion from './hero-section-secondary-illustration.png'
 import gameTitleIllustration from './game-title-illustration.webp'
 import gameFightSceneIllustration from './game-fight-scene-illustration.png'
-import Logo512 from './migawe-logo-512.png'
+import Logo512Dark from './migawe-logo-512-dark.png'
+import Logo512Light from './migawe-logo-512-light.png'
 import gameRules from './rules.svg'
 export {
   heroSectionPrimaryIllustartion,
   heroSectionSecondaryIllustartion,
   gameTitleIllustration,
   gameFightSceneIllustration,
-  Logo512,
+  Logo512Dark,
+  Logo512Light,
   gameRules,
 }
